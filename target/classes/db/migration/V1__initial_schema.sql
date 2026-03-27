4444dc262ab010fa106f795c7a9e191c91bbd781
andreaterrasi <andrea.terrasi78@gmail.com>
2026-03-27 08:59:29 +0100

    Harden Kubernetes deployment defaults

--- target/classes/db/migration/V1__initial_schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE newsletter_preference (
-    id BIGSERIAL PRIMARY KEY,
-    customer_id BIGINT NOT NULL UNIQUE,
-    subscribed BOOLEAN NOT NULL DEFAULT FALSE,
-    updated_at TIMESTAMPTZ NOT NULL
-);
-
-CREATE INDEX idx_newsletter_customer_id ON newsletter_preference(customer_id);
